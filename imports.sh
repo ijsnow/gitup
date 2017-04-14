f6d9d559174428ea204c26d7f0297c3790b1a504
Isaac Snow <isaacjsnow@gmail.com>
2017-04-14 17:03:08 -0500

    stuff

--- imports.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-go list -f '{{join .Deps "\n"}}' > deps.txt
-go list -f '{{join .TestImports "\n"}}' >> deps.txt
-sort deps.txt -u -o deps.txt
-go list std | sort | comm -23 ./deps.txt - | grep -v `go list` |  xargs -I{} $GOPATH/bin/gvt fetch {}
-rm ./deps.txt
